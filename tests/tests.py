d7856a41faec99f94bc17fec1f3ea526ff795b0c
AlexandrKruglov <krulalex84@gmail.com>
2024-02-15 04:03:18 +0300

    создали 2 класса написали тесты к классам установили pytest

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-import my_class
+from src import my_class
 
 
 @pytest.fixture
@@ -13,13 +13,14 @@ def exmp_2():
     return my_class.Product('lg', 'good tv', 5.5, 6)
 
 
-def test_init_category(exmp_1):
+def tests_init_category(exmp_1):
     assert exmp_1.name == 'tv'
     assert exmp_1.description == 'look tv'
     assert exmp_1.products == ['rubin', 'lg']
     assert exmp_1.count_name == 1
     assert exmp_1.count_products == 2
-def test_init_product(exmp_2):
+
+def tests_init_product(exmp_2):
     assert exmp_2.name == 'lg'
     assert exmp_2.description == 'good tv'
     assert exmp_2.price == 5.5
